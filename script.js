1c9bd869c24361a231b9a3d9b0d370532fe975d3
João Pedro <joaopetrussousa@gmail.com>
2022-04-28 10:36:31 -0300

    Requisito 10-incompleto

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ const elementoVerde = document.getElementById("green");
 
 elementoPreto.classList.add("selected");
 
+let tamanho;
+window.onload = criaBoard(tamanho)
+function criaBoard(){
+if(tamanho == undefined){
+    tamanho = 5;
+}
+for(let i=0; i<tamanho; i++){
+    let criaColuna = document.createElement("div");
+    let pixelBoard = document.getElementById("pixel-board");
+    pixelBoard.appendChild(criaColuna);
+    criaColuna.className = "board"
+}
+const criaPixel = document.createElement("div");
+    
+for(let i=0; i<tamanho; i++){
+    for(let j=0; j<tamanho; j++){
+    let criaLinha = document.createElement("div");
+    let boardChild = document.getElementsByClassName("board");
+    boardChild[i].appendChild(criaLinha);
+    criaLinha.className = "pixel"
+    }
+    }
+}
+
 function colorSelected(event){
     if(event.target == elementoVermelho){
         elementoVermelho.classList.add("selected");
@@ -55,4 +79,17 @@ function limpar(event){
     for(let pixel of pixels){
         pixel.style.backgroundColor = "white";
     }
+}
+
+const botaoGerar = document.getElementById("generate-board");
+botaoGerar.addEventListener("click", gerar);
+
+function gerar(event){
+    let input = document.querySelector("#board-size");
+    let size = input.value; 
+    if(size === ""){        
+        window.alert("Board inválido!");
+    }
+    tamanho = size;
+    criaBoard(tamanho)
 }
